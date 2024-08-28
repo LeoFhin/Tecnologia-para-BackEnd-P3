a1590faf1ad72e0491a2c07dbb3a215e9cdac094
Leonardo Santos De Lima <LeonardoSantos@academicos.com>
2024-08-27 21:15:47 -0300

    Mesclando as branchs e criando o repository e sevice

--- src/main/java/br/com/alunoonline/api/model/Aluno.java
+++ src/main/java/br/com/alunoonline/api/model/Aluno.java
@@ -1,2 +1,24 @@
-package br.com.alunoonline.api.model;public class Aluno {
+package br.com.alunoonline.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Aluno implements Serializable {
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+    private String nome;
+    private String email;
+    private String cpf;
 }
